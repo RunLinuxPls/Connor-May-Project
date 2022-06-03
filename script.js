43aaa00d500ab958c621def0dff3ae845b1f6a4e
Andrew Loeffler <drewloeffler@gmail.com>
2022-06-03 04:25:52 -0400

    add previous button, title, styling

--- script.js
+++ script.js
@@ -2,19 +2,39 @@
 'use strict';
 import { SeventySentences } from "./SentencesbyCristianPopescu.js"
 import { shuffleArray } from "./Fisher-Yates_shuffle.js"
-function loadTextSnippetsFromJSON() {
+
+(function loadTextSnippetsFromJSON() {
     const poemArray = SeventySentences.body,
-        appWrapper = document.querySelector("#appWrapper")
-    poemArray.forEach((each) => { appWrapper.appendChild(document.createElement("p")).appendChild(document.createTextNode(each)) })
-    document.querySelectorAll("#appWrapper p").forEach((each) => { each.className = "sentenceCard" })
-}
-loadTextSnippetsFromJSON()
-let shuffledElementsArray = shuffleArray([...document.querySelectorAll("#appWrapper p")])
+        cardWrapper = document.querySelector("#cardWrapper")
+    poemArray.forEach((each) => { cardWrapper.appendChild(document.createElement("p")).appendChild(document.createTextNode(each)) })
+    document.querySelectorAll("#cardWrapper p").forEach((each) => { each.className = "sentenceCard" })
+})()
+
+let shuffledElementsArray = shuffleArray([...document.querySelectorAll("#cardWrapper p")]) // Init static nodeList, shuffle, init array
 let currentIndex = 0
-document.querySelectorAll("#appWrapper p").forEach((each) => { each.style.display = "none" })
-shuffledElementsArray[currentIndex].style.display = "grid"
+document.querySelectorAll("#cardWrapper p").forEach((each) => { each.style.display = "none" }); // Hide all the cards
+shuffledElementsArray[currentIndex].style.display = "grid"; // Display the first card
 document.querySelector("#nextCard").onclick = () => {
-    shuffledElementsArray[currentIndex].style.display = "none"
-    currentIndex ++
-    shuffledElementsArray[currentIndex].style.display = "grid"
+    if (currentIndex < shuffledElementsArray.length - 1) {
+        shuffledElementsArray[currentIndex].style.display = "none"
+        currentIndex++
+        shuffledElementsArray[currentIndex].style.display = "grid"
+    }
+    else if (currentIndex === shuffledElementsArray.length - 1) {
+        shuffledElementsArray[currentIndex].style.display = "none"
+        currentIndex = 0
+        shuffledElementsArray[currentIndex].style.display = "grid"
+    }
+}
+document.querySelector("#previousCard").onclick = () => {
+    if (currentIndex > 0) {
+        shuffledElementsArray[currentIndex].style.display = "none"
+        currentIndex--
+        shuffledElementsArray[currentIndex].style.display = "grid"
+    }
+    else if (currentIndex === 0) {
+        shuffledElementsArray[currentIndex].style.display = "none"
+        currentIndex = shuffledElementsArray.length - 1
+        shuffledElementsArray[currentIndex].style.display = "grid"
+    }
 }
